64360c2679457988c8ae7607d97d09be6fbce95a
Pedro Marchal <pedro.marchal@innocv.com>
2018-01-20 13:03:16 +0100

    add list to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Item } from './shared/list/list.model';
+import { DataService } from './shared/data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  public items: Array<Item>;
+
+  constructor(private _dataService: DataService) {
+    this.items = _dataService.getItems();
+  }
+
+  changeList(num: number) {
+
+  }
 
 }
